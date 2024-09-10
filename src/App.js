632227da53db78d0925a2ff6167c439d03c118b3
Nivethikan Neethirasa <107372637+Neethirasa@users.noreply.github.com>
2024-09-10 11:19:32 -0400

    fixed deployment issue

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Navigation from './components/Navigation';
+import Navigation from './components/Navigation'; 
 
 function App() {
   return (
-    <Router>
+    <Router basename="/my-portfolio">  {/* Set the basename */}
       <div>
-        <Navigation /> {/* Navigation bar displayed on all pages */}
-        <Routes> {/* Replace 'Switch' with 'Routes' */}
-          <Route path="/" element={<Home />} /> {/* Update to use 'element' prop */}
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />  {/* Home Page */}
         </Routes>
       </div>
     </Router>
